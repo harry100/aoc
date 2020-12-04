1b555f9587dba59b0fe863415875ccb57bb8b40c
harry100 <maharjanamanda@gmail.com>
2020-12-04 20:07:19 +0545

    cleanup

--- day2/main.py
+++ day2/main.py
@@ -1,35 +1,31 @@
 from data import data
 
 def get_values(l):
-    case = {}
-    case['matchVal'] = l[0].split(" ")[1]
-    case['password'] = l[1]
-    case['minOccurence'] = int(l[0].split(" ")[0].split("-")[0])
-    case['maxOccurence'] = int(l[0].split(" ")[0].split("-")[1])
-    return case
+    formula_val, value = l.split(":")
+    recipe, match_value = formula_val.split(" ")
+    min_occurence, max_occurence = recipe.split("-")
+    return [ value, match_value, int(min_occurence), int(max_occurence) ]
+
 
 #Sol 1
-validPasswords = 0
-for d in data:
-    c = get_values(d.split(":"))
-    count = c['password'].count(c['matchVal'])
-    min = c['minOccurence']
-    max = c['maxOccurence']
-    if(count >= min):
-        if(count <= max):
-            validPasswords += 1
+valid_password_count = 0
 
-print(validPasswords)
+for d in data:
+    value, match_value, min_occurence, max_occurence = get_values(d)
+    count = value.count(match_value)
+    if(count >= min_occurence):
+        if(count <= max_occurence):
+            valid_password_count += 1
+
+print(valid_password_count)
 
 
 # Sol 2
-validPasswordLength = 0
-for d in data:
-    c = get_values(d.split(":"))
-    min = c['minOccurence']
-    max = c['maxOccurence']
-    matchChar = c['matchVal']
-    if ((c['password'][min] == matchChar) != (c['password'][max] == matchChar)):
-        validPasswordLength += 1
+valid_password_count1 = 0
 
-print(validPasswordLength)
+for d in data:
+    value, match_value, min_occurence, max_occurence = get_values(d)
+    if ((value[min_occurence] == match_value) != (value[max_occurence] == match_value)):
+        valid_password_count1 += 1
+
+print(valid_password_count1)
