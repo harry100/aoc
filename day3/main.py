b517dbb065608c0bdeb588357564d0af83aa4bcf
Amanda Maharjan <maharjanamanda@gmail.com>
2020-12-05 22:22:49 +0545

    solution 2 and cleanup

--- day3/main.py
+++ day3/main.py
@@ -3,19 +3,27 @@ with open('data.txt', 'r') as f:
     data = paths.split('\n')
 
 tree = "#"
-trees_count, index = 0, 0
 
-for p in data:
-    str(p)
-    while True:
-        try:
-            tree_or_space = p[index]
-            if(tree_or_space == tree):
-                print(tree_or_space, tree)
-                trees_count += 1
-            index += 3
-            break
-        except IndexError:
-            p = p + p
+def trees_encounter(left_moves, down_moves):
+    trees_count, index, list_len = 0, 0, len(data)
 
-print(trees_count)
+    for i in range(0, list_len, down_moves):
+        p = data[i]
+        while True:
+            try:
+                tree_or_space = p[index]
+                if(tree_or_space == tree):
+                    print(tree_or_space, tree)
+                    trees_count += 1
+                index += left_moves
+                break
+            except IndexError:
+                p += p
+    
+    return trees_count
+
+trees = trees_encounter(3, 1)
+
+print(trees)
+
+print(trees_encounter(1, 1) * trees_encounter(3, 1) * trees_encounter(5, 1) * trees_encounter(7, 1) * trees_encounter(1, 2))
