cacef033847c8e0fb1c75bad8492306d47ed57b1
Amanda Maharjan <maharjanamanda@gmail.com>
2020-12-15 16:50:09 +0000

    second soln attempt

--- day6/main.py
+++ day6/main.py
@@ -11,3 +11,33 @@ def get_yesses(data=data):
 
 get_yesses()
 print(sum(list_data))
+
+matches = []
+
+def get_matches(group):
+    l = len(group)
+    for i in range(0, len(group)):
+        for char in group[i]:
+            if (char in group[i]):
+                if(i != 0):
+                    matches.append(char)
+            else:
+                matches.remove(char)
+    # return matches
+
+
+# split_data = []
+
+def get_all_yesses(data=data):
+    for d in data:
+        s = d.split("\n")
+        get_matches(s)
+        # return len(v)
+
+        # print(s)
+        # split_data.append(s)
+
+get_all_yesses()
+print(len(matches))
+
+# print(split_data)unique_1 = [w for w in set(word_1) if w in word_2]
