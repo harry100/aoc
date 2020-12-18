0b0577e87434afe61a252ef211fe1a405a2e5050
Amanda Maharjan <maharjanamanda@gmail.com>
2020-12-18 13:23:30 +0000

    part 2 soln

--- day6/main.py
+++ day6/main.py
@@ -13,31 +13,27 @@ get_yesses()
 print(sum(list_data))
 
 matches = []
+set_list, c = [], 0
 
 def get_matches(group):
-    l = len(group)
-    for i in range(0, len(group)):
-        for char in group[i]:
-            if (char in group[i]):
-                if(i != 0):
-                    matches.append(char)
-            else:
-                matches.remove(char)
-    # return matches
-
+    set_group = []
+    for g in group:
+        set_group.append(set(g))
+    set_list.append(set_group)
 
 # split_data = []
 
 def get_all_yesses(data=data):
     for d in data:
-        s = d.split("\n")
+        s = d.split("\n",)
         get_matches(s)
-        # return len(v)
-
-        # print(s)
-        # split_data.append(s)
 
 get_all_yesses()
-print(len(matches))
 
-# print(split_data)unique_1 = [w for w in set(word_1) if w in word_2]
+for s in set_list:
+    ic = set.intersection(*s)
+    print(ic)
+    c = c + len(ic)
+print(c)
+
+# print(split_data)unique_1 = [w for w in set(word_1) if w in word_2]1
